a19e289e4df42c2f162591be938f0743289b3a03
Michael Boyd <Ucmikeboyd5@gmail.com>
2015-06-13 16:14:42 -0500

    Updated the default num to 0

--- src/SumCalcuator.java
+++ src/SumCalcuator.java
@@ -1,7 +1,7 @@
 
 public class SumCalcuator {
 	public static void main(String[] myArgs){
-		int sum = 0;
+		int sum = 1;
 		for(int i = 0; i<myArgs.length; i++){
 			sum += Integer.parseInt(myArgs[i]);
 		}
